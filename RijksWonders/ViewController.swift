a433caf082b0268763e889636816ba95570e3389
Maxim Khatskevich <maxim@khatskevi.ch>
2021-12-01 18:56:15 +0100

    [ADD] iPhone app target

---
+++ RijksWonders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RijksWonders
+//
+//  Created by Maxim Khatskevich on 01/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
