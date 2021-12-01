0fc39cf35dbcc6e3b03fca2baf6ee04921777e5e
Maxim Khatskevich <maxim@khatskevi.ch>
2021-12-01 23:28:21 +0100

    [ADD] `Services` layer

---
+++ RijksWondersServices/RijksWondersServices.h
@@ -0,0 +1,18 @@
+//
+//  RijksWondersServices.h
+//  RijksWondersServices
+//
+//  Created by Maxim Khatskevich on 01/12/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RijksWondersServices.
+FOUNDATION_EXPORT double RijksWondersServicesVersionNumber;
+
+//! Project version string for RijksWondersServices.
+FOUNDATION_EXPORT const unsigned char RijksWondersServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RijksWondersServices/PublicHeader.h>
+
+
