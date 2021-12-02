2b1f4e431fbc401b8b37c3ce40bd750d98848500
Maxim Khatskevich <maxim@khatskevi.ch>
2021-12-02 03:29:20 +0100

    [ADD] `ViewModels` layer

---
+++ RijksWondersViewModels/RijksWondersViewModels.h
@@ -0,0 +1,18 @@
+//
+//  RijksWondersViewModels.h
+//  RijksWondersViewModels
+//
+//  Created by Maxim Khatskevich on 02/12/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RijksWondersViewModels.
+FOUNDATION_EXPORT double RijksWondersViewModelsVersionNumber;
+
+//! Project version string for RijksWondersViewModels.
+FOUNDATION_EXPORT const unsigned char RijksWondersViewModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RijksWondersViewModels/PublicHeader.h>
+
+
